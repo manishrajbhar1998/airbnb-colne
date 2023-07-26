b736e9ba7c53f5c86f013dcf6f73fae5c52b2f91
manishrajbhar1998 <manishrajbhar379@gmail.com>
2023-07-27 00:19:24 +0530

    add place start

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const  User = require('./models/user');
+const User = require('./models/user');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
 require('./data');
 
 const server = express();
@@ -14,53 +15,63 @@ server.use(cors());
 server.use(express.json());
 server.use(cookieParser());
 
-server.post('/register',async (req,res)=>{
-    const {name,email,password} = req.body.user
-    try{
+server.post('/register', async (req, res) => {
+    const { name, email, password } = req.body.user
+    try {
         const userDoc = await User.create({
             name,
             email,
-            password:bcrypt.hashSync(password,bcryptSalt)
+            password: bcrypt.hashSync(password, bcryptSalt)
         })
-        res.json({success:true,data:userDoc});
-    }catch(err){
-        res.status(422).json({success:false,error:err.message});
+        res.json({ success: true, data: userDoc });
+    } catch (err) {
+        res.status(422).json({ success: false, error: err.message });
     }
 })
 
-server.post('/login', async (req, res)=>{
-    try{
-        const {email,password} = req.body.user;
-        const userDoc = await User.findOne({email})
-        if(userDoc){
-            const passOk = bcrypt.compareSync(password.toString(),userDoc.password);
-            if(passOk){
-                jwt.sign({email:userDoc.email,id:userDoc._id,name:userDoc.name},jwtSecret,{},(err,token)=>{
-                    if(err) throw err;
-                    res.cookie('token',token).json({success: true, data:userDoc,token:token});
+server.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body.user;
+        const userDoc = await User.findOne({ email })
+        if (userDoc) {
+            const passOk = bcrypt.compareSync(password.toString(), userDoc.password);
+            if (passOk) {
+                jwt.sign({ email: userDoc.email, id: userDoc._id, name: userDoc.name }, jwtSecret, {}, (err, token) => {
+                    if (err) throw err;
+                    res.cookie('token', token).json({ success: true, data: userDoc, token: token });
                 })
-            }else{
-                res.status(422).json({success: false, message:"credentials are not correct"});
+            } else {
+                res.status(422).json({ success: false, message: "credentials are not correct" });
             }
-        }else{
-            res.status(404).json({success: false, message:"not found"})
+        } else {
+            res.status(404).json({ success: false, message: "not found" })
         }
-    }catch(err){
-        console.log("error login :: ",err);
+    } catch (err) {
+        console.log("error login :: ", err);
     }
 })
 
-server.post('/profile', (req,res)=>{
+server.post('/profile', (req, res) => {
     // const {token} = req.cookies;
-    const {token} =  req.body;
-    if(token){
-        jwt.verify(token,jwtSecret,{},(err, user)=>{
-            if(err) throw err;
+    const { token } = req.body;
+    if (token) {
+        jwt.verify(token, jwtSecret, {}, (err, user) => {
+            if (err) throw err;
             res.json(user);
         })
     }
 });
 
-server.listen(5500,()=>{
+server.post('/upload-by-link', async (req, res) => {
+    const { link } = req.body;
+    const newName = Date.now() + '.jpg';
+    await imageDownloader.image({
+        url: link,
+        dest: __dirname + '/upload' + newName
+    })
+    res.json(__dirname + '/upload' + newName)
+})
+
+server.listen(5500, () => {
     console.log("listening on 5000")
 })
